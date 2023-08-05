ba3c4ece70a79952c6f982f2da9b345469b1fe9a
SaiyedRushan <rushan52@gmail.com>
2023-08-05 18:26:36 -0400

    init firebase for auth

--- Productivus-Frontend/src/main.js
+++ Productivus-Frontend/src/main.js
@@ -1,9 +1,20 @@
 import App from "./App.vue";
 import { createApp } from "vue";
 import { registerPlugins } from "@/plugins";
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const app = createApp(App);
-
 registerPlugins(app);
-
 app.mount("#app");
