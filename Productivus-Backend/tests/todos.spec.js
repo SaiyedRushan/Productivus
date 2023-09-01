a439d84720306fb8365332ab2aaa4fafdbef4725
SaiyedRushan <rushan52@gmail.com>
2023-09-01 01:23:28 -0400

    added standard linting

--- Productivus-Backend/tests/todos.spec.js
+++ Productivus-Backend/tests/todos.spec.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-undef */
 const app = require("../index");
 const supertest = require("supertest");
 const request = supertest(app);
 
 describe("GET /", function () {
-  it("responds with status 200", function (done) {
-    request.get("/api/").expect(200, done);
+  it("responds with status 200", async function (done) {
+    await request.get("/api/").expect(200, done);
   });
 });
